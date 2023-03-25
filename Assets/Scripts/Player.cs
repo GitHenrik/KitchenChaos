c5acd20ff082ae0bcbf553e6873ca79c02722138
Henrik <henrik.tarnanen@gmail.com>
2023-03-25 18:24:39 +0200

    add clear counter and basic raycasting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,47 @@ public class Player : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 7f;
   [SerializeField] private GameInput gameInput;
+  [SerializeField] private LayerMask countersLayerMask;
 
   private bool isWalking;
+  private Vector3 lastInteractDir;
+
   private void Update()
+  {
+    HandleMovement();
+    HandleInteractions();
+  }
+
+  public bool IsWalking()
+  {
+    return isWalking;
+  }
+
+  private void HandleInteractions()
+  {
+    Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+    Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+    if (moveDir != Vector3.zero)
+    {
+      lastInteractDir = moveDir;
+    }
+
+    float interactDistance = 2f;
+    // RaycastHit: Structure used to get information back from a raycast
+    // Variables passed as "out" arguments do not have to be initialized before being passed in a method call
+    // .Raycast has several useful overloads for different situations
+    if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+    {
+      // tryGetComponent performs null check (vs. GetComponent)
+      if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+      {
+        clearCounter.Interact();
+      }
+    }
+  }
+
+  private void HandleMovement()
   {
     Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -68,10 +106,5 @@ public class Player : MonoBehaviour
     isWalking = moveDir != Vector3.zero;
     float rotateSpeed = 10f;
     transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-  }
-
-  public bool IsWalking()
-  {
-    return isWalking;
   }
 }
