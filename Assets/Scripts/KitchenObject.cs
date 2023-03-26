8d40a6eec669aed8751ad352eaa329d8a5a1fed0
Henrik <henrik.tarnanen@gmail.com>
2023-03-26 13:22:31 +0300

    add logic for for counter-object-relations

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,9 +5,37 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
   [SerializeField] private KitchenObjectSO kitchenObjectSO;
+  private ClearCounter clearCounter;
 
   public KitchenObjectSO GetKitchenObjectSO()
   {
     return kitchenObjectSO;
+  }
+
+  public void SetClearCounter(ClearCounter clearCounter)
+  {
+    // if previous counter exists, reset that counter's state to NOT have anything on it
+    if (this.clearCounter != null)
+    {
+      this.clearCounter.ClearKitchenObject();
+    }
+    // update the counter reference to the new counter
+    this.clearCounter = clearCounter;
+
+    if (clearCounter.HasKitchenObject())
+    {
+      Debug.LogError("Counter already has a KitchenObject!");
+    }
+
+    clearCounter.SetKitchenObject(this);
+
+    // update visuals
+    transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+    transform.localPosition = Vector3.zero;
+  }
+
+  public ClearCounter GetClearCounter()
+  {
+    return clearCounter;
   }
 }
