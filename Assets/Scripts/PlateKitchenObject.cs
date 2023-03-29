844c1effb426379c11b57dca6ee93373c7400298
Henrik <henrik.tarnanen@gmail.com>
2023-03-29 21:40:38 +0300

    add filled plate visuals

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+
+  public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+  public class OnIngredientAddedEventArgs : EventArgs
+  {
+    public KitchenObjectSO kitchenObjectSO;
+  }
   [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
   private List<KitchenObjectSO> kitchenObjectSOList;
@@ -28,6 +35,7 @@ public class PlateKitchenObject : KitchenObject
     else
     {
       kitchenObjectSOList.Add(kitchenObjectSO);
+      OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { kitchenObjectSO = kitchenObjectSO });
       return true;
     }
   }
