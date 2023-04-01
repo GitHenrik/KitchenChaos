87e3437268e1e30c48b7c9c596a58f145d691118
Henrik <henrik.tarnanen@gmail.com>
2023-04-01 14:47:32 +0300

    add game state manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,14 +45,20 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
   private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
   {
+    if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
     if (selectedCounter != null)
     {
       selectedCounter.InteractAlternate(this);
     }
+
+
   }
 
   private void GameInput_OnInteractAction(object sender, System.EventArgs e)
   {
+    if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
     if (selectedCounter != null)
     {
       selectedCounter.Interact(this);
